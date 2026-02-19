ea85aec5c9986079ec2571a71382da0cf666c9ce
reman16 <remanadhikari16@gmail.com>
2026-02-18 19:41:24 -0700

    Added secrurity details

--- PasswordChecker.java
+++ PasswordChecker.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 public class PasswordChecker {
    
     public static void main(String[] args){
-        System.out.println("Hello");
+       Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a password to Check: ");
+        String password = scanner.nextLine();
+
+        int passwordScore = calculateScore(password);
+        String passwordStrength = getStrength(passwordScore);
+
+        System.out.println("\nPassword Score: " + passwordScorescore + "/100");
+        System.out.println("Strength: " + passwordStrengthstrength);
     }   
+
+    public static int calculateScore(String password){
+        
+    }
 }
