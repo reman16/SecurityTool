ed4ff6575fb76a02a50814b2fedd625dac4c56ba
reman16 <remanadhikari16@gmail.com>
2026-02-19 19:31:50 -0700

    Updated the methods

--- PasswordChecker.java
+++ PasswordChecker.java
@@ -10,11 +10,53 @@ public class PasswordChecker {
         int passwordScore = calculateScore(password);
         String passwordStrength = getStrength(passwordScore);
 
-        System.out.println("\nPassword Score: " + passwordScorescore + "/100");
-        System.out.println("Strength: " + passwordStrengthstrength);
+        System.out.println("Strength: " + passwordStrength);
     }   
 
-    public static int calculateScore(String password){
-        
+    public static String getStrength(int score){
+
+        if(score < 30){
+            return "Weak";
+        }
+        else if(score < 60){
+            return "Moderate";
+        }
+        else if(score < 80){
+            return "Strong";
+        }
+        else{
+            return "Very Strong";
+        }
     }
+    
+    public static int calculateScore(String password){
+    
+        int score = 0;
+    
+        if(password.length() < 6){
+            score += 10;
+        } 
+        else if(password.length() < 10){
+            score += 30;
+        } 
+        else {
+            score += 50;
+        }
+        if(password.matches(".*[A-Z].*")){
+            score += 15;
+        }
+        if(password.matches(".*[a-z].*")){
+            score += 15;
+        }
+        if(password.matches(".*[0-9].*")){
+            score += 10;
+        }
+        if(password.matches(".*[^a-zA-Z0-9].*")){
+            score += 10;
+        }
+    
+        return score;
+    }
+    
 }
+
